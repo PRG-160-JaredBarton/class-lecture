690547210d0000bf69282d9950eb16c4efc6df5b
jaredbarton2000 <jb47516@ravens.racc.edu>
2018-02-09 22:34:32 -0500

    added variable scope into test.js

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
 function getAverage(a,b,c,d,e,f){
 
-var average = (a + b + c + d + e + f) / 6;
+var average = (a + b + c + d + e + f) / 6; //local varible
 console.log(average);
 return average;
 
 
 }
 
-var myResult = getAverage(7,3,6,8,13,10);
-console.log("the average is") + myResult;
+var myResult = getAverage(7,3,6,8,13,10); //global varible
+
+function logResult(){
+
+console.log("the average is" + myResult);
+
+
+}
+
+logResult();
