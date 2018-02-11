fd8a471957f9e7c6abd580685acf589ec197457c
jaredbarton2000 <jb47516@ravens.racc.edu>
2018-02-11 12:36:07 -0500

    added jsref split to the test.js file

--- test.js
+++ test.js
@@ -1,19 +1,3 @@
-function getAverage(a,b,c,d,e,f){
-
-var average = (a + b + c + d + e + f) / 6; //local varible
-console.log(average);
-return average;
-
-
-}
-
-var myResult = getAverage(7,3,6,8,13,10); //global varible
-
-function logResult(){
-
-console.log("the average is" + myResult);
-
-
-}
-
-logResult();
+var str = "This is an amazing sentence.";
+var words = str.split(" ");
+console.log(words);
