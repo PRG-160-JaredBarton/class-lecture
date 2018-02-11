bbe433799402ba379d9c4ac6e225e1519664d0ce
jaredbarton2000 <jb47516@ravens.racc.edu>
2018-02-11 13:09:16 -0500

    added script to find if number is prime or not

--- test.js
+++ test.js
@@ -1,1 +1,23 @@
-document.getElementById("myChatter").innerHTML = "This is my first website.";
+function test_prime(n)
+{
+
+  if (n===1){
+    document.write ("Not Prime")
+    return false;
+  }
+  else if(n === 2){
+    return true;
+  }else{
+    document.write ("Prime")
+    for(var x = 2; x < n; x++)
+    {
+      if(n % x === 0)
+      {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+console.log(test_prime(37));
